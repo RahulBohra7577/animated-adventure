cd5d4afc36e6eef6c79278ebeb379f3b30b06d46
RahulBohra7577 <80052781+RahulBohra7577@users.noreply.github.com>
2021-05-23 21:35:22 +0530

    Add files via upload

---
+++ Timeclass.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<ostream>
+#include<cstring>
+
+using namespace std;
+class Time
+{
+private :
+     int hours;
+     int minutes;
+
+ public:
+     void getTime()
+     {
+         cout<<"ENTER TIME :"<<endl;
+         cout<<"HOURS :";
+         cin>>hours;
+         cout<<endl<<"MINUTES :";
+         cin>>minutes;
+     }
+
+     void printTime()
+     {
+         cout<<endl<<"COURENT TIME IS :";
+         cout<<hours<<":"<<minutes;
+         cout<<endl;
+
+     }
+
+     void addTime(Time T1,Time T2)
+     {
+         this->minutes=T1.minutes+T2.minutes;
+         this->hours=T1.hours+T2.hours+this->minutes/60;
+         this->minutes %=60;
+         this->hours %=24;
+     }
+
+      void putTime()
+     {
+         cout<<endl<<"TIME AFTER ADD :";
+         cout<<hours<<":"<<minutes<<endl;
+
+     }
+
+};
+
+int main()
+{
+    Time T1,T2,T3;
+    T1.getTime();
+    T1.printTime();
+    T2.getTime();
+    T3.addTime(T1,T2);
+    T3.putTime();
+
+    return 0;
+}
